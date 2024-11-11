7f7ec61bbb616df17405937296288275486cd2d5
Jeefry <frisk7530@gmail.com>
2024-11-10 23:25:39 -0500

    Modify MapScreen (markets personalizados)

--- app/src/main/java/com/puyodev/xlaboratorio12/MapScreen.kt
+++ app/src/main/java/com/puyodev/xlaboratorio12/MapScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
@@ -16,10 +18,20 @@ import com.google.maps.android.compose.Marker
 @Composable
 fun MapScreen() {
     val ArequipaLocation = LatLng(-16.4040102, -71.559611) // Arequipa, Perú
-    val cameraPositionState = rememberCameraPositionState {
+
+    val locations = listOf(
+        LatLng(-16.433415,-71.5442652), // JLByR
+        LatLng(-16.4205151,-71.4945209), // Paucarpata
+        LatLng(-16.3524187,-71.5675994) // Zamacola
+    )
+
+    /*val cameraPositionState = rememberCameraPositionState {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
     }
-
+*/
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
+    }
 
     Box(modifier = Modifier.fillMaxSize()) {
         // Añadir GoogleMap al layout
@@ -27,11 +39,22 @@ fun MapScreen() {
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Añadir marcador en Denver, Colorado
+            /*
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
+                icon = BitmapDescriptorFactory.fromResource(R.drawable.spaceinvader2), // Icono personalizado
                 title = "Arequipa, Perú"
             )
+            */
+            locations.forEach { location ->
+                Marker(
+                    state = rememberMarkerState(position = location),
+                    title = "Ubicación",
+                    icon = BitmapDescriptorFactory.fromResource(R.drawable.spaceinvader2), // Icono personalizado
+                    snippet = "Punto de interés"
+                )
+            }
+
         }
     }
 }
